1d23b04a100f3503e1b85f750bd15b36984ccf14
Ishaan Chamoli <email@ishaanchamoli.com>
2024-11-17 18:18:42 -0600

    back to working

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,6 @@ import { SessionProvider } from "next-auth/react";
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Head>
-        <title>VibeGPT</title>
-        <link rel="icon" href="/logotab.png" />
-      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
