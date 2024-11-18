2276cd3cd99f8d2ba2119b42097257ed61be83e8
Ishaan Chamoli <email@ishaanchamoli.com>
2024-11-17 18:16:41 -0600

    Add title and favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,10 @@ import { SessionProvider } from "next-auth/react";
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>VibeGPT</title>
+        <link rel="icon" href="/logotab.png" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
